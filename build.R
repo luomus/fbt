0595a2c19b2344f6f1cf8915e7ad5d0c53756d37
wkmor1 <willi@mmorris.email>
2025-06-09 13:46:11 +0300

    Get data from schema.laji.fi

--- build.R
+++ build.R
@@ -1,23 +1,105 @@
 use("glue", "glue")
-unlink("terms.md")
+use("jsonlite", "fromJSON")
+unlink("index.html")
+
+cat(
+  sprintf(
+    '<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <style>body{background-color:white;}</style>
+
+  <title>FinBIF list of terms</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="FinBIF list of terms">
+  <link rel="schema.DC" href="http://purl.org/DC/elements/1.0/">
+  <meta name="DC.Title" content="FinBIF terms">
+  <meta name="DC.Description" content="FinBIF list of terms">
+  <meta name="DC.Date" content=%s>
+  <meta name="DC.Language" content="en">
+  <meta name="DC.Subject" content="biodiversity data">
+</head>
+<body>
+  <main>
+',
+    Sys.Date()
+  ),
+  file = "index.html",
+  append = TRUE
+)
 
 term_template <- readLines("term.template")
 
-terms <- read.csv("terms.csv")
-terms <- apply(terms, 1, c, simplify = FALSE)
+terms <- c(
+  "informalTaxonGroup",
+  "vernacularNameSwedish",
+  "redListStatusFinland",
+  "lajiturvaStatus",
+  "regulatoryStatuses",
+  "taxonPreferredHabitat",
+  "isSensitive",
+  "datasetCurationLevel",
+  "qualityControl",
+  "qualityIssues",
+  "sourceName",
+  "orginatingSource",
+  "biogeographcialProvince",
+  "locationStatus",
+  "pairCount",
+  "isMigrating",
+  "isBreedingSite",
+  "invasiveTaxonManagementAction",
+  "isStateLand",
+  "coordinateAccuracyClass",
+  "gridCellYKJ"
+)
 
-for (i in terms) {
+for (term in terms) {
 
-  term       <- i[["term"]]
-  label      <- i[["label"]]
-  definition <- i[["definition"]]
-  notes      <- i[["notes"]]
-  examples   <- i[["examples"]]
-
-  cat(
-    as.character(do.call(glue, c(as.list(term_template), .sep = "\n"))),
-    file = "index.html",
-    append = TRUE
+  term_data <- try(
+    fromJSON(
+      sprintf("https://rs.laji.fi/terms/%s?format=json", term),
+      simplifyVector = FALSE
+    ),
+    TRUE
   )
 
+  if (!inherits(term_data, "try-error")) {
+
+    lbl <- term_data[["label"]]
+    lbl <- lbl[[which(vapply(lbl, "[[", "", "@language") == "en")]][["@value"]]
+    lbl <- lbl %||% ""
+
+    dfn <- term_data[["HBDF.definition"]]
+    dfn <- dfn[[which(vapply(dfn, "[[", "", "@language") == "en")]][["@value"]]
+    dfn <- lbl %||% ""
+
+    nts <- term_data[["HBDF.notes"]]
+    nts <- nts[[which(vapply(nts, "[[", "", "@language") == "en")]][["@value"]]
+    nts <- nts %||% ""
+
+    exl <- paste(term_data[["HBDF.examples"]], collapse = "</br>")
+    exl <- exl %||% ""
+
+    cat(
+      as.character(
+        do.call(glue, c(as.list(term_template), .sep = "\n", .trim = FALSE))
+      ),
+      file = "index.html",
+      append = TRUE
+    )
+
+  }
 }
+
+
+cat(
+  '
+  </main>
+</body>
+</html>
+',
+  file = "index.html",
+  append = TRUE
+)
